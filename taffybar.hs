55fad4d5466fb37780c7309d4d22887db71c20dc
Patriot720 <cerkin-3@yandex.ru>
2022-05-31 08:22:28 +0300

    update 08:22 Tue, 31 May

--- taffybar.hs
+++ taffybar.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import ClickableWidget
 import Control.Monad.Trans.Class
 import Data.Map (insert)
 import Data.Maybe
 import Data.Ratio
+import qualified Data.Text as T
+import SimpleCmd
 import StatusNotifier.Tray
 import System.Taffybar
 import System.Taffybar.Information.CPU
@@ -17,30 +18,9 @@ import System.Taffybar.Widget.Generic.Graph
 import System.Taffybar.Widget.Generic.PollingGraph
 import System.Taffybar.Widget.Generic.PollingLabel
 
-cpuCallback = do
-  (_, systemLoad, totalLoad) <- cpuLoad
-  return [totalLoad, systemLoad]
-
-getFullWorkspaceNames :: X11Property [(WorkspaceId, String)]
-getFullWorkspaceNames = go <$> readAsListOfString Nothing "_NET_DESKTOP_FULL_NAMES"
-  where
-    go = zip [WorkspaceId i | i <- [0 ..]]
-
-workspaceNamesLabelSetter workspace =
-  remapNSP . fromMaybe "" . lookup (workspaceIdx workspace)
-    <$> liftX11Def [] getFullWorkspaceNames
-  where
-    remapNSP "NSP" = "S"
-    remapNSP n = n
-
 main = do
-  let cpuCfg =
-        defaultGraphConfig
-          { graphDataColors = [(0, 1, 0, 1), (1, 0, 1, 0.5)]
-          }
-      -- \61463
+  let -- \61463
       clock = textClockNewWith $ defaultClockConfig {clockFormatString = "%H:%M %a, %d %b"}
-      -- cpu = pollingGraphNew cpuCfg 0.5 cpuCallback
       workspaces =
         workspacesNew
           defaultWorkspacesConfig
@@ -49,48 +29,22 @@ main = do
               getWindowIconPixbuf = scaledWindowIconPixbufGetter getWindowIconPixbufFromEWMH,
               urgentWorkspaceState = True
             }
+      tray = sniTrayNewFromParams defaultTrayParams
 
-      -- workspaces =
-      --   flip widgetSetClassGI "workspaces"
-      --     =<< workspacesNew
-      --       defaultWorkspacesConfig
-      --         { minIcons = 1,
-      --           getWindowIconPixbuf =
-      --             scaledWindowIconPixbufGetter $
-      --               getWindowIconPixbufFromChrome
-      --                 <|||> unscaledDefaultGetWindowIconPixbuf
-      --                 <|||> (\size _ -> lift $ loadPixbufByName size "application-default-icon"),
-      --           widgetGap = 0,
-      --           showWorkspaceFn = hideEmpty,
-      --           updateRateLimitMicroseconds = 100000
-      --         }
-      vpn =
-        clickableWidget
-          ClickableWidgetConfig
-            { pollingPeriod = 2,
-              command = "~/.config/polybar/wg-status.sh --status",
-              onLeftClick = "~/.config/polybar/wg-status.sh",
-              onFail = "Failed"
-            }
-      tray =
-        sniTrayNewFromParams
-          defaultTrayParams
       simpleConfig =
         defaultSimpleTaffyConfig
           { startWidgets =
-              [ workspaces
+              [
+                workspaces
               ],
             barHeight = ScreenRatio $ 1 / 35,
             centerWidgets =
               [ clock
               ],
             endWidgets =
-              [ -- cpu,
-                sniTrayNew,
-                commandRunnerNew 6000 "curl" ["-s", "https://wttr.in/?format=%c%20%t%20(%f)"] "Fail",
-                vpn
+              [ sniTrayNew
+                -- commandRunnerNew 6000 "curl" ["-s", "https://wttr.in/?format=%c%20%t%20(%f)"] "Fail"
               ],
-            barPosition = Bottom,
-            monitorsAction = useAllMonitors
+            barPosition = Bottom
           }
   simpleTaffybar simpleConfig
