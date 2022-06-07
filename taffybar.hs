b311d28a244e6b727988f369b36414c977444dd1
Patriot720 <cerkin-3@yandex.ru>
2022-06-07 22:00:07 +0300

    update 22:00 Tue, 07 Jun

--- taffybar.hs
+++ taffybar.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Class
 import Data.Map (insert)
 import Data.Maybe
@@ -7,6 +8,7 @@ import Data.Ratio
 import qualified Data.Text as T
 import SimpleCmd
 import StatusNotifier.Tray
+import System.IO (IOMode (ReadMode), openFile, withFile, hSetEncoding, utf8_bom, hGetContents)
 import System.Taffybar
 import System.Taffybar.Information.CPU
 import System.Taffybar.Information.EWMHDesktopInfo
@@ -18,6 +20,14 @@ import System.Taffybar.Widget.Generic.Graph
 import System.Taffybar.Widget.Generic.PollingGraph
 import System.Taffybar.Widget.Generic.PollingLabel
 import System.Taffybar.Widget.WttrIn
+
+twitchChat :: IO T.Text
+twitchChat =
+  do
+    h <- openFile "/home/andrew/build/twitch-chat-cli/test.txt" ReadMode
+    hSetEncoding h utf8_bom
+    text <- hGetContents h
+    return $ T.pack $ last $ lines text
 
 main = do
   let -- \61463
@@ -39,14 +49,20 @@ main = do
               ],
             barHeight = ScreenRatio $ 1 / 35,
             centerWidgets =
-              [clock
+              [ clock
               ],
             endWidgets =
               [ sniTrayNew,
-              pollingLabelNew 0.1 (T.pack <$> shell "tail -n 1 /home/andrew/build/twitch-chat-cli/test.txt | ansifilter -M -f")
-              -- textWttrNew "http://wttr.in/?format=%c%20%t%20(%f)" 60
-
+                -- commandRunnerNew
+                --   0.1
+                --   "tail"
+                --   ["-n", "1", "/home/andrew/build/twitch-chat-cli/test.txt"]
+                --   "FAIL"
+                  pollingLabelNew 0.1 twitchChat
+                  -- textWttrNew "http://wttr.in/?format=%c%20%t%20(%f)" 60
               ],
             barPosition = Bottom
           }
   simpleTaffybar simpleConfig
+
+--  | ansifilter -M -f
