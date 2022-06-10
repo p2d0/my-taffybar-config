de471773a97068613388ad43538cf8cc1002c549
Patriot720 <cerkin-3@yandex.ru>
2022-06-10 22:00:12 +0300

    update 22:00 Fri, 10 Jun

--- taffybar.hs
+++ taffybar.hs
@@ -1,14 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Control.Exception (SomeException, handle)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Class
 import Data.Map (insert)
 import Data.Maybe
 import Data.Ratio
 import qualified Data.Text as T
+import GHC.IO.Encoding (setLocaleEncoding)
+import Network.HTTP.Types.URI
 import SimpleCmd
 import StatusNotifier.Tray
-import System.IO (IOMode (ReadMode), openFile, withFile, hSetEncoding, utf8_bom, hGetContents)
+import System.IO (IOMode (ReadMode), hGetContents, hSetEncoding, openFile, utf8, utf8_bom, withFile)
+import System.Log.Logger
 import System.Taffybar
 import System.Taffybar.Information.CPU
 import System.Taffybar.Information.EWMHDesktopInfo
@@ -24,9 +28,7 @@ import System.Taffybar.Widget.WttrIn
 twitchChat :: IO T.Text
 twitchChat =
   do
-    h <- openFile "/home/andrew/build/twitch-chat-cli/test.txt" ReadMode
-    hSetEncoding h utf8_bom
-    text <- hGetContents h
+    text <- readFile "/home/andrew/build/twitch-chat-cli/test.txt"
     return $ T.pack $ last $ lines text
 
 main = do
@@ -49,21 +51,24 @@ main = do
               ],
             barHeight = ScreenRatio $ 1 / 35,
             centerWidgets =
-              [ clock
-                  -- textWttrNew "http://wttr.in/Moscow?format=1" 6000
+              [ clock,
+                commandRunnerNew 1800 "curl" ["-s", "https://wttr.in/?format=%c%20%t%20(%f)"] "Fail"
               ],
             endWidgets =
               [ sniTrayNew,
-                -- commandRunnerNew
-                --   0.1
-                --   "tail"
-                --   ["-n", "1", "/home/andrew/build/twitch-chat-cli/test.txt"]
-                --   "FAIL"
-                  pollingLabelNew 0.1 twitchChat,
-                  textWttrNew "pepega" 60
+                pollingLabelNew 0.1 twitchChat
               ],
             barPosition = Bottom
           }
-  simpleTaffybar simpleConfig
+  setLocaleEncoding utf8
+  handle logException $ simpleTaffybar simpleConfig
 
---  | ansifilter -M -f
+-- Logs an Http Exception and returns wttr.in's weather unknown label.
+logException :: SomeException -> IO ()
+logException e = do
+  let errmsg = show e
+  logM
+    "Pepega"
+    ERROR
+    ("Warning: Couldn't call wttr.in. \n" ++ errmsg)
+  return ()
